3f189ea941ba8124079711182ed3c234acb12eaf
Basmapep <basmapep@gmail.com>
2025-09-15 08:09:45 +0530

    vaiant added and env file changed

--- model/productVariantModel.js
+++ model/productVariantModel.js
@@ -4,6 +4,7 @@ const productVariantSchema = new mongoose.Schema({
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: [true, 'Category is required'] },
     productVariantName: { type: String, requied: [true, 'productVariant Name is required'] },
     productVariantDescription: { type: String },
+    productMaterial: { type: String, requied: [true, 'Material is required'] }
 })
 
 module.exports = mongoose.model('ProductVariant', productVariantSchema)
