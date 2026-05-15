8501c0552a95253108c1486789191d2bccc672f7
samsudhan23 <samsudhan23@gmail.com>
2026-05-16 01:50:01 +0530

    forgot password change to build url

--- routes/auth.js
+++ routes/auth.js
@@ -118,7 +118,7 @@ router.post('/auth/forgot-password', async (req, res) => {
         user.resetTokenExpiry = Date.now() + 3600000; // 1 hour
         await user.save();
 
-        const resetUrl = `http://localhost:4200/reset-password/${token}`;
+        const resetUrl = `https://final-gc-cloths-1hwj.vercel.app/reset-password/${token}`;
         const html = `<p>Click the link below to reset your password:</p><a href="${resetUrl}">${resetUrl}</a>`;
 
         await sendOTPMail(user.email, html, 'Reset Password', false);
